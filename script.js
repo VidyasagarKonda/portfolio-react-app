8df17f837c996f7163bc63834dbdde6d665e2cbd
VidyasagarKonda <vidyasagar.vsk77@gmail.com>
2025-03-17 11:01:11 +0530

    updated send message

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+document.getElementById('contact-form').addEventListener('submit', async function(event) {
+  event.preventDefault(); // Prevent default form submission
+  
+  let formData = new FormData(this);
+  let response = await fetch('https://formspree.io/f/xanenjqg', {
+      method: 'POST',
+      body: formData,
+      headers: { 'Accept': 'application/json' }
+  });
+  
+  if (response.ok) {
+      alert('Message sent successfully!');
+      this.reset(); // Reset form after successful submission
+  } else {
+      alert('Error sending message. Please try again later.');
+  }
+});
